6ba4283cc0d6ed2549b9b3c980b491aa83f8caac
Caroline Andrade <dev.caroline.ribeiro@gmail.com>
2024-03-21 23:55:40 -0300

    Projeto finalizado

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,35 +1,37 @@
-import {useState} from 'react'
+import React, { useState } from 'react';
 
-const TodoForm = () => {
+const TodoForm = (props) => {
+    const { addTodo } = props; // Recebendo addTodo do objeto props
 
-    const[value, setValue] = useState("");
-    const[category, setCategory] = useState("");
+    const [value, setValue] = useState("");
+    const [category, setCategory] = useState("");
 
     const handleSubmit = (e) => {
-      e.preventDefault();
-      if(!value || !category) return;
+        e.preventDefault();
+        if (!value || !category) return;
 
-      //adicionar todo
-      //limpar campos
-      setValue("");
-      setCategory("");
+        //adicionar todo
+        addTodo(value, category);
+        //limpar campos
+        setValue("");
+        setCategory("");
     };
 
-  return (
-    <div className='todo-form'>
-        <h2>Criar Tarefa: </h2>
-        <form onSubmit={handleSubmit}>
-            <input type='text' placeholder='Digite o título' value={value} onChange={(e) => setValue(e.target.value)}/>
-            <select value={category} onChange={(e) => setCategory(e.target.value)}>
-                <option value=''>Selecione uma categoria</option>
-                <option value='Trabalho'>Trabalho</option>
-                <option value='Pessoal'>Pessoal</option>
-                <option value='Estudos'>Estudos</option>
-            </select>
-            <button className='tarefa' type='submit'>Criar tarefa</button>
-        </form>
-    </div>
-  )
+    return (
+        <div className='todo-form'>
+            <h2>Criar Tarefa: </h2>
+            <form onSubmit={handleSubmit}>
+                <input type='text' placeholder='Digite o título' value={value} onChange={(e) => setValue(e.target.value)} />
+                <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                    <option value=''>Selecione uma categoria</option>
+                    <option value='Trabalho'>Trabalho</option>
+                    <option value='Pessoal'>Pessoal</option>
+                    <option value='Estudos'>Estudos</option>
+                </select>
+                <button className='tarefa' type='submit'>Criar tarefa</button>
+            </form>
+        </div>
+    )
 }
 
-export default TodoForm
+export default TodoForm;
